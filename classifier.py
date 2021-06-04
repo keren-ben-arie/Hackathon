ec66c5af9fd085060e7a148ccd3a896edd477347
dana-ad <71030798+dana-ad@users.noreply.github.com>
2021-06-04 10:31:53 +0300

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -68,11 +68,11 @@ def send_police_cars(X):
     train = train_co[["X Coordinate", "Y Coordinate", "hour", "minutes"]].dropna()
     winners_to_return = []
     for date in X:
-        kmeans = KMeans(n_clusters=200)
+        kmeans = KMeans(n_clusters=30)
         coords_KMeans = kmeans.fit(train)
         lables = coords_KMeans.labels_
         cluster_centers = coords_KMeans.cluster_centers_
-        score = dict(zip(range(200), np.zeros(200)))
+        score = dict(zip(range(30), np.zeros(30)))
         for i in range(len(lables)):
             p0 = cluster_centers[lables[i]]
             p1 = train.iloc[i]
